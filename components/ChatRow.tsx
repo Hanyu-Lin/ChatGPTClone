812e5f8feb3a3dfc0fe8c5844d6ac1124a61dc4c
Hanyu-Lin <hanyulin02@gmail.com>
2023-02-18 15:09:10 +0100

    added remove chat functionality

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -4,20 +4,29 @@ import {
   ChatBubbleBottomCenterTextIcon,
   TrashIcon,
 } from "@heroicons/react/24/outline";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../firebase";
+import { useSession } from "next-auth/react";
 
 type Props = {
   id: string;
 };
 
 function ChatRow({ id }: Props) {
+  const router = useRouter();
   const pathname = usePathname();
   const [active, setActive] = useState(false);
-
+  const { data: session } = useSession();
   useEffect(() => {
     if (!pathname) return;
     setActive(pathname.includes(id));
   }, [pathname]);
+
+  const removeChat = async () => {
+    await deleteDoc(doc(db, "users", session?.user?.email!, "chats", id));
+    router.replace("/");
+  };
   return (
     <div>
       <Link
@@ -26,7 +35,10 @@ function ChatRow({ id }: Props) {
       >
         <ChatBubbleBottomCenterTextIcon className="h-5 w-5 text-white" />
         <p className="flex-1 hiden md:inline-flex truncate ">New Chat</p>
-        <TrashIcon className="h-5 w-5 text-gray-500 hover:text-red-500/80" />
+        <TrashIcon
+          onClick={removeChat}
+          className="h-5 w-5 text-gray-500 hover:text-red-500/80"
+        />
       </Link>
     </div>
   );
--- components/NewChatButton.tsx
+++ components/NewChatButton.tsx
@@ -1,31 +1,32 @@
-'use client'
-import { PlusIcon } from '@heroicons/react/24/solid'
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+"use client";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-import React from 'react'
-import { db } from '../firebase';
+import React from "react";
+import { db } from "../firebase";
 
 function NewChatButton() {
   const router = useRouter();
-  const {data : session}= useSession();
-  const createNewChat = async() => {
+  const { data: session } = useSession();
+  const createNewChat = async () => {
     const doc = await addDoc(
-      collection(db,"users", session?.user?.email!, "chats"),{
+      collection(db, "users", session?.user?.email!, "chats"),
+      {
         userId: session?.user?.email!,
+        messages: [],
         createdAt: serverTimestamp(),
       }
     );
-    router.push(`/chat/${doc.id}`)
+    router.push(`/chat/${doc.id}`);
   };
   return (
-    <div onClick={createNewChat} className='border border-gray-700 chatRow'>
-      <PlusIcon className='h-4 w-4'></PlusIcon>
+    <div onClick={createNewChat} className="border border-gray-700 chatRow">
+      <PlusIcon className="h-4 w-4"></PlusIcon>
       <p> New Chat</p>
-
     </div>
-  )
+  );
 }
 
-export default NewChatButton
+export default NewChatButton;
