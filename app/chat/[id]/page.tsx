a236918fb6c3af0d9c11f696c508162d7d397179
Hanyu-Lin <hanyulin02@gmail.com>
2023-02-18 20:48:02 +0100

    created layout for users chatPage

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import ChatInput from "../../../components/ChatInput";
+import ChatWindow from "../../../components/ChatWindow";
 
-function ChatPage() {
+type Props = {
+  params: {
+    id: string;
+  };
+};
+function ChatPage({ params: { id } }: Props) {
   return (
-    <div>ChatPage</div>
-  )
+    <div className="flex flex-col h-screen overflow-hidden">
+      {/* Message window */}
+      <ChatWindow chatId={id}></ChatWindow>
+
+      {/* input area */}
+      <ChatInput chatId={id}></ChatInput>
+    </div>
+  );
 }
 
-export default ChatPage
+export default ChatPage;
