91d13f7ec7b255a213129b858e751a503972bd45
Hanyu-Lin <hanyulin02@gmail.com>
2023-02-18 12:44:14 +0100

    Added log button to the sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,10 @@
+'use client'
+import { signOut, useSession } from 'next-auth/react'
 import React from 'react'
 import NewChatButton from './NewChatButton'
+import {ArrowRightOnRectangleIcon} from '@heroicons/react/24/outline'
 function SideBar() {
+  const {data : session}= useSession();
   return (
     <div className='flex flex-col p-2 h-screen'>
       <div className='flex-1'>
@@ -12,8 +16,15 @@ function SideBar() {
             {/* select models */}
           </div>
             {/* the chat list, all the rows*/}
+
         </div>
-      </div>    
+      </div> 
+      {session && ( 
+        <button className=' logOutButton' onClick={() => signOut()}>
+          <ArrowRightOnRectangleIcon className=' h-5 w-5'/>
+          <p>Log Out</p>
+        </button>
+        )}   
     </div>
   )
 }
