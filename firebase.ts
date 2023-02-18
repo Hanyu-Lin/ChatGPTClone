2d0ee3fd942c40c6e4e567e6b010d50e8f75691a
Hanyu-Lin <hanyulin02@gmail.com>
2023-02-18 20:46:51 +0100

    initialize firebase app on the client and server side

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,6 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,11 +11,11 @@ const firebaseConfig = {
   projectId: "chatgptclone-f6bd2",
   storageBucket: "chatgptclone-f6bd2.appspot.com",
   messagingSenderId: "834290088576",
-  appId: "1:834290088576:web:5d67812f468ec7f549c465"
+  appId: "1:834290088576:web:5d67812f468ec7f549c465",
 };
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig); //singelton
 const db = getFirestore(app);
 
-export {db}
+export { db };
