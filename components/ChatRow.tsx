5e722916113ac880b1e131c7d866dab20af91141
Hanyu-Lin <hanyulin02@gmail.com>
2023-02-19 17:26:08 +0100

    bug fixing: wrong orderBy should be desc

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { ChatBubbleLeftIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { usePathname, useRouter } from "next/navigation";
-import { deleteDoc, doc } from "firebase/firestore";
+import { collection, deleteDoc, doc, orderBy, query } from "firebase/firestore";
 import { db } from "../firebase";
 import { useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 type Props = {
   id: string;
@@ -25,6 +26,13 @@ function ChatRow({ id }: Props) {
     await deleteDoc(doc(db, "users", session?.user?.email!, "chats", id));
     router.replace("/");
   };
+  const [messages] = useCollection(
+    query(
+      collection(db, "users", session?.user?.email!, "chats", id, "messages"),
+      orderBy("createdAt", "desc")
+    )
+  );
+  const latestMessages = messages?.docs[0]?.data().text;
   return (
     <div>
       <Link
@@ -32,7 +40,9 @@ function ChatRow({ id }: Props) {
         className={`chatRow justify-center $ ${active && "bg-gray-700/30"}`}
       >
         <ChatBubbleLeftIcon className="h-5 w-5 text-white" />
-        <p className="flex-1 hiden md:inline-flex truncate ">New Chat</p>
+        <p className="flex-1 hiden md:inline-flex truncate ">
+          {latestMessages || "New Chat"}
+        </p>
         <TrashIcon
           onClick={removeChat}
           className="h-5 w-5 text-gray-500 hover:text-red-500/80"
